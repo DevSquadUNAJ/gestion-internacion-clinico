6cfb60bac52b02ab9aaaaeaaa9386e6713c094f9
AgustinMartinez03 <agustincarpmartinez@gmail.com>
2026-06-19 19:53:53 -0300

    feat(infra): implementar capa anticorrupcion con refit para consumir admision e inyeccion automatica de tokens jwt

---
+++ Clinico.Infraestructura/ServiciosExternos/AdmisionServicio.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+using Clinico.Aplicacion.DTOs.Respuestas.Admision;
+using Clinico.Aplicacion.Interfaces.IExternos;
+using Clinico.Infraestructura.Refit;
+
+namespace Clinico.Infraestructura.ServiciosExternos
+{
+    public class AdmisionServicio : IAdmisionServicio
+    {
+        private readonly IAdmisionApi _admisionApi;
+
+        public AdmisionServicio(IAdmisionApi admisionApi)
+        {
+            _admisionApi = admisionApi;
+        }
+
+        public async Task<ContextoInternacionRespuesta> ObtenerContextoInternacionAsync(Guid internacionId)
+        {
+            // Aquí en un futuro se puede agregar un bloque try-catch
+            // para atrapar Refit.ApiException y lanzar excepciones propias de Clínico si Admisión se cae.
+            // Por ahora, lo mantenemos simple.
+            return await _admisionApi.ObtenerContextoInternacionAsync(internacionId);
+        }
+    }
+}
